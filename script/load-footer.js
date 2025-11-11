1e1519785dd8ccdeda8c37d82f0a36381a8e5422
Minh hieu Duong <hieubatrieu2006@gmail.com>
2025-11-10 21:02:10 -0500

    add more progress, not yet finished, still need shop page, product page, and some effect to make it better

---
+++ script/load-footer.js
@@ -0,0 +1,36 @@
+document.getElementById("footer").innerHTML = `
+    <footer>
+        <div class="footer">
+            <div class="wrapper">
+                <section class="footer-content">
+                    <div class="footer-logo footer-child">
+                        <div class="footer-logo">
+                            <div class="logo">
+                                <img src="img/logo.png" alt="Logo" class="nav-logo">
+                                <p class="logo-text">Skate Shop</p>
+                            </div>
+                            <p>Premium skateboards and accessories for street riders and park shredders. Built by skaters, for skaters since 2015.</p>
+                        </div>
+                    </div>
+                    <div class="footer-nav footer-child">
+                        <p class="footer-highlight">Quick Link</p>
+                        <ul class="nav-menu-footer">
+                            <li><a href="./index.html">Home</a></li>
+                            <li><a href="./about.html">About</a></li>
+                            <li><a href="">Shop</a></li>
+                            <li><a href="">Contact</a></li>
+                        </ul>
+                    </div>
+                    <div class="contact-info footer-child">
+                        <p class="footer-highlight">Contact Info</p>
+                        <ul>
+                            <li>456 Skate Street, Los Angeles, CA 90012</li>
+                            <li>(555) 987-6543</li>
+                            <li>info@urbanshred.com</li>
+                        </ul>
+                    </div>
+                </section>
+            </div>
+        </div>
+    </footer>
+`;
---
+++ script/load-header.js
@@ -0,0 +1,20 @@
+document.getElementById("header").innerHTML = `
+    <header>
+        <div class="wrapper">
+            <section class="header-content">
+                <div class="logo">
+                    <img src="img/logo.png" alt="Logo" class="nav-logo">
+                    <p class="logo-text">Skate Shop</p>
+                </div>
+                <nav>
+                    <ul class="nav-menu">
+                        <li><a href="./index.html">Home</a></li>
+                        <li><a href="./about.html">About</a></li>
+                        <li><a href="./shop.html">Shop</a></li>
+                        <li><a href="./contact.html">Contact</a></li>
+                    </ul>
+                </nav>
+            </section>
+        </div>
+    </header>
+`;
